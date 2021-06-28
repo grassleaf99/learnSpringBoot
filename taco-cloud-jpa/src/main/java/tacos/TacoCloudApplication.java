74a6d56d9044a62cc5418b7dc86ff7e7b735c7d1
grassleaf99 <65818460+grassleaf99@users.noreply.github.com>
2021-06-28 21:38:55 +0700

    Spring Boot data JPA

---
+++ taco-cloud-jpa/src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,19 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan("tacos")
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
---
+++ taco-cloud-jpa/src/main/java/tacos/data/OrderRepository.java
@@ -0,0 +1,11 @@
+package tacos.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import tacos.Order;
+
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long>{
+
+}
